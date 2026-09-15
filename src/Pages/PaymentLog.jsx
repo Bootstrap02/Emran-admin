15414dff5ada449230e2c1a9f04b8e37dbf7678d
Louis Ogochukwu Udegbue <143167253+Bootstrap02@users.noreply.github.com>
2026-09-15 08:26:52 +0100

    Update PaymentLog.jsx

--- src/Pages/PaymentLog.jsx
+++ src/Pages/PaymentLog.jsx
@@ -3,7 +3,7 @@
 
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { FiDownload, FiRefreshCw, FiCheckCircle, FiXCircle, FiClock, FiAlertCircle } from 'react-icons/fi';
+import { FiDownload, FiRefreshCw, FiCheckCircle, FiXCircle, FiClock, } from 'react-icons/fi';
 
 const API = 'https://campusbuy-backend-nkmx.onrender.com/mobilcreatepayment';
 
